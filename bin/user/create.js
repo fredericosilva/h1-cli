52a14477320e180fbab2a14d60b68bcf7090c1d5
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-05-30 16:15:01 +0200

    Fix dash in user create

--- bin/user/create.js
+++ bin/user/create.js
@@ -8,71 +8,71 @@ const inquirer = require('inquirer');
 
 const verification = {
     email: {}
-  , phone: {}
+    , phone: {}
 };
 
 const options = {
     email: {
         description: 'email'
-      , type: 'string'
-      , defaultValue: {
+        , type: 'string'
+        , defaultValue: {
             interactive: () => inquirer.prompt({
                 type: 'input'
-              , name: 'value'
-              , message: 'email:'
-              , validate: input => _.isEmpty(input) ? 'Invalid email' : true
+                , name: 'value'
+                , message: 'email:'
+                , validate: input => _.isEmpty(input) ? 'Invalid email' : true
             })
         }
     }
-  , emailCode: {
+    , 'email-code': {
         description: 'email validation code'
-      , type: 'string'
-      , defaultValue: {
+        , type: 'string'
+        , defaultValue: {
             interactive: ctx => ctx.args.helpers.api.post(`verification/email/${ctx.args.email}`)
                 .then(emailVerification => verification.email = emailVerification)
                 .then(() => inquirer.prompt({
                     type: 'input'
-                  , name: 'value'
-                  , message: 'email validation:'
-                  , validate: input => _.isEmpty(input) ? 'Invalid code' : true
+                    , name: 'value'
+                    , message: 'email validation:'
+                    , validate: input => _.isEmpty(input) ? 'Invalid code' : true
                 }))
         }
     }
-  , phone: {
+    , phone: {
         description: 'phone'
-      , type: 'string'
-      , defaultValue: {
+        , type: 'string'
+        , defaultValue: {
             interactive: () => inquirer.prompt({
                 type: 'input'
-              , name: 'value'
-              , message: 'phone number:'
-              , validate: input => _.isEmpty(input) ? 'Invalid phone number' : true
+                , name: 'value'
+                , message: 'phone number:'
+                , validate: input => _.isEmpty(input) ? 'Invalid phone number' : true
             })
         }
     }
-  , phoneCode: {
+    , 'phone-code': {
         description: 'phone validation code'
-      , type: 'string'
-      , defaultValue: {
+        , type: 'string'
+        , defaultValue: {
             interactive: ctx => ctx.args.helpers.api.post(`verification/phone/${ctx.args.phone}`)
                 .then(phoneVerification => verification.phone = phoneVerification)
                 .then(() => inquirer.prompt({
                     type: 'input'
-                  , name: 'value'
-                  , message: 'phone validation:'
-                  , validate: input => _.isEmpty(input) ? 'Invalid code' : true
+                    , name: 'value'
+                    , message: 'phone validation:'
+                    , validate: input => _.isEmpty(input) ? 'Invalid code' : true
                 }))
         }
     }
-   , password: {
+    , password: {
         description: 'Password'
-      , type: 'string'
-      , defaultValue: {
+        , type: 'string'
+        , defaultValue: {
             interactive: () => inquirer.prompt({
                 type: 'password'
-              , name: 'value'
-              , message: 'password:'
-              , validate: input => _.isEmpty(input) ? 'Incorrect password' : true
+                , name: 'value'
+                , message: 'password:'
+                , validate: input => _.isEmpty(input) ? 'Incorrect password' : true
             })
         }
     }
@@ -81,10 +81,10 @@ const options = {
 const handler = args => args.helpers.api
     .post('user', {
         email: args.email
-      , password: args.password
-      , verification: {
-            email: Object.assign(verification.email, { code: args.emailCode })
-          , phone: Object.assign(verification.phone, { code: args.phoneCode })
+        , password: args.password
+        , verification: {
+            email: Object.assign(verification.email, {code: args["email-code"]})
+            , phone: Object.assign(verification.phone, {code: args["phone-code"]})
         }
     })
     .then(() => logger('info', 'User successfully created!'))
@@ -92,10 +92,10 @@ const handler = args => args.helpers.api
 
 module.exports = Cli.createCommand('create', {
     description: 'Create an account'
-  , plugins: [
+    , plugins: [
         require('bin/_plugins/api')
-      , require('bin/_plugins/interactiveOptions')
+        , require('bin/_plugins/interactiveOptions')
     ]
-  , options: options
-  , handler: handler
+    , options: options
+    , handler: handler
 });
